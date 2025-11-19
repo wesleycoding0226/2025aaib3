77d16a9177ddf039b29472c090161d857a163744
wesleycoding0226 <chenwesley531@gmail.com>
2025-11-19 11:51:03 +0800

    week03

---
+++ week03/week03-2b.cpp
@@ -0,0 +1,16 @@
+//WEEK03-2b.cpp
+//C++
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string a;
+	cin >> a;
+	int bad = 0;
+	for(int i=0; i<4; i++){
+		if(a[i] != a[4-1-i]) bad = 1;
+	}
+	if(bad==1) cout << "NO\n";
+	else cout << "YES\n";;
+}
