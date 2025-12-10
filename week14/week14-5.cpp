f1a6d77416660ee720352233d0e5adf5c95a7cdd
wesleycoding0226 <chenwesley531@gmail.com>
2025-12-10 11:49:06 +0800

    week14

---
+++ week14/week14-5.cpp
@@ -0,0 +1,12 @@
+/// week14-5.cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    while (cin >> N){
+        if(N<0) break;
+        int ans = 1 + (1+N)*N/2;
+        cout << ans << "\n" ;
+    }
+}
