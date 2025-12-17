6ee955c3aba6b06645beea9f4a88658fe47d09c5
wesleycoding0226 <chenwesley531@gmail.com>
2025-12-17 11:34:41 +0800

    week15

---
+++ week15/week15-5.cpp
@@ -0,0 +1,11 @@
+//week14-5.cpp
+class Solution {
+public:
+
+    int maxDepth(TreeNode* root) {
+        if(root==nullptr)return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left,right) + 1;
+    }
+};
